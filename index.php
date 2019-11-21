0f9745fe3b9222f687e423b4503c41b017a6b633
jasiel13 <jasielutt@gmail.com>
2019-11-21 13:15:44 -0600

    primer commit del sitio web

---
+++ index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="css/estilos.css"></link>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|PT+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+
+    <!--librerias para crear animaciones-->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <script type="text/javascript" src="wowjs/wow.min.js"></script>
+    <script type="text/javascript">new WOW().init();</script> 
+    <link rel="stylesheet" type="text/css" href="Hover/css/hover-min.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+</head>
+<body>
+<button type="button" class="up" style="position: fixed; bottom:20px; right:20px; display:none;"><i class="fa fa-arrow-up" style="color: #fff;"></i></button> 
+<a id="whatsapp" href="https://api.whatsapp.com/send?phone=528180184512" target="_blank"><img src="img/whatsapp.png" alt="whatsapp" class="imgwha"></a>
+    <header>
+        <div class="menu">
+            <div class="contenedor">
+                <p class="logo wow bounceInLeft"><strong><a href="#">Jasiel Becerra</strong></a></p>
+                <img class="menu-icon" src="img/menu2.png" alt="">
+                <nav class="wow bounceInRight">
+                    <ul>
+                        <li><a href="#sobre-mi" class="newcolor">Sobre mi</a></li>
+                        <li><a href="#servicios" class="newcolor">Servicios</a></li>
+                        <li><a href="#mis-trabajos" class="newcolor">Mis Trabajos</a></li>
+                        <li><a href="#por-que-trabajar" class="newcolor">¿Por que trabajar conmigo?</a></li>
+                        <li><a href="#blog" class="newcolor">Blog</a></li>
+                        <li><a href="#contacto" class="newcolor">Contacto</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="contenedor" id="contenedor-titulo-flex">
+            <div class="contenedor-titulo wow bounceInDown">
+                <h1>Jasiel Becerra</h1>
+                <h2>Diseñador & Desarrollador web</h2>
+                <a href="#mis-trabajos" class="hvr-float">Ver mis Trabajos</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section id="sobre-mi">
+            <div class="contenedor">
+                <h3>Sobre mi</h3>
+                <div class="contenedor-sobremi">
+                    <div class="computadoras wow rotateInUpLeft">
+                        <img src="img/computadoras.png">
+                    </div>
+                    <div class="texto wow rotateInUpRight">
+                        <p align="justify">Mi nombre es Jasiel Becerra soy Ingeniero en Tecnologías de la información y comunicación. 
+                        Me desempeño en diversas áreas de informática como lo es la administración de redes & servidores, soporte técnico,
+                         y estoy empezando aprender nuevas tecnologías para orquestar contenedores y micro servicios.</p>
+                        <p align="justify">También desarrollo contenido web, tengo experiencia en diseño gráfico, creación de sitios web y tiendas online para pequeñas y medianas empresas. 
+                        Me aseguro de utilizar siempre las últimas tecnologías de desarrollo web:
+                        Html5, Css3, Jquery, Javascript, Ajax, Bootstrap, Angular,Php.</p>
+                        <p align="justify"> Si necesitas una página web moderna, funcional, que se adapte a diferentes dispositivos, 
+                        te aseguro que llegaste al lugar correcto.
+                        Puedes ponerte en contacto conmigo a través del formulario de contacto que se encuentra al final de mi sitio web.
+                        </p>
+                        <a href="#contacto" class="hvr-wobble-top" align="center">Contáctame</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="servicios">
+                <div class="contenedor">
+                  <h3>Servicios</h3>
+                  <div class="contenedor-servicios wow slideInUp">
+                    <div class="servicio violeta">
+                      <h4>Diseño Web</h4>
+                      <p align="justify">Páginas web atractivas, funcionales en base a las necesidades del cliente. La importancia del diseño es fundamental para tener éxito y atraer a tus visitantes. También lo es la velocidad de carga del sitio web, deben ser rápidos para que el visitante tenga la mejor experiencia.</p>
+                      <img class="icono" src="img/monitor.png">
+                      <img class="ondas" src="img/waves.png">
+                    </div>
+                    <div class="servicio celeste">
+                      <h4>Responsive Design</h4>
+                      <p align="justify">Hoy en día las páginas web se ven en multitud de dispositivos como tablets,
+                         celulares, libros electrónicos, notebooks, PCs, etc. 
+                         La tecnología móvil es el presente y el futuro. 
+                         Por eso es importante y me aseguro de que el sitio web sea compatible con todos los tamaños de pantalla de esta época moderna.</p>
+                      <img class="icono" src="img/mobile.png">
+                      <img class="ondas" src="img/waves.png">
+                    </div>
+                    <div class="servicio violeta">
+                      <h4>SEO</h4>
+                      <p align="justify">Al crear sitios web, se tiene muy en cuenta la optimización de motores de búsqueda (SEO). 
+                        Se llevan a cabo prácticas de SEO estándar en cada página del sitio web, lo que permite que el sitio tenga una clasificación más alta en los resultados de Google y asi generar mas tráfico a la web.</p>
+                      <img class="icono" src="img/seo.png">
+                      <img class="ondas" src="img/waves.png">
+                    </div>
+                    <div class="servicio celeste">
+                      <h4>Autoadministrable</h4>
+                      <p align="justify">Es importante que el cliente pueda administrar la página el mismo, 
+                        desde cualquier lugar y en cualquier momento. Se desarrollan sistemas de gestión de contenido que te permiten ver los datos de la página web y editar el contenido que se dese, lo que permite mantenerse actualizado.</p>
+                      <img class="icono" src="img/wordpress.png">
+                      <img class="ondas" src="img/waves.png">
+                    </div>
+                    <div class="servicio violeta">
+                      <h4>Mantenimiento</h4>
+                      <p align="justify">Puedes contactarme si necesitas actualizar tu página web,
+                         agregar nuevas secciones o cualquier cambio que necesites.
+                          Esto es perfecto para los sitios web de pequeñas empresas, donde no se necesita un diseñador de páginas web a tiempo completo, sino solo en distintas ocasiones.</p>
+                      <img class="icono" src="img/mante.png">
+                      <img class="ondas" src="img/waves.png">
+                    </div>
+                    <div class="servicio celeste">
+                      <h4>Marketing Digital</h4>
+                      <p align="justify">Se utilizan las herramientas mas importantes como Google Adwords y Google Analytics, 
+                        para que tu negocio aparezca en los resultados de búsqueda de Google justo cuando alguien te necesita y analizar como los usuarios interactúan con tu página web.</p>
+                      <img class="icono" src="img/marketing.png">
+                      <img class="ondas" src="img/waves.png">
+                    </div>
+                  </div>
+                </div>
+              </section> 
+              
+              <section id="mis-trabajos">
+                <div class="contenedor">
+                  <h3>Mis Trabajos</h3>
+                    <div class="owl-carousel owl-theme wow rotateIn">
+                        <div class="item">
+                          <a href="https://www.reybec.com/" target="_blank">
+                          <img src="img/reybectech.png">
+                          <p>Reybec Technologies</p>
+                          </a>
+                        </div>
+                        <div class="item">
+                          <a href="http://www.begiaccesorios.com/" target="_blank">
+                            <img src="img/begiaccesorios.png">
+                            <p>Begi Accesorios</p>
+                          </a>  
+                        </div>
+                          <div class="item">
+                            <a href="">
+                              <img src="img/diseño-web.png">
+                              <p>Páginas web</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="">
+                              <img src="img/controldeflotilla.png">
+                              <p>Sistema Control de Flota</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="">
+                              <img src="img/csn.png">
+                              <p>Sistema de Facturación</p>
+                            </a>
+                        </div>
+                        <div class="item">
+                            <a href="">
+                              <img src="img/diseños.png">
+                              <p>Diseño Gráfico</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+              </section>
+
+              <section id="por-que-trabajar">
+                  <div class="contenedor">
+                    <h3>¿Por que trabajar conmigo?</h3>
+                    <div class="contenedor-trabajar wow zoomIn">
+                      <div class="item">
+                        <ul>
+                          <li><img src="img/velocidad.png" alt=""></li>
+                          <li>Velocidad</li>
+                          <li>Me aseguro de que tu web cargue entre 2 y 4 segundos (depende de la cantidad de contenido del sitio). Si una web tarda más de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar más.</li>
+                        </ul>
+                      </div>
+                      <div class="item">
+                        <ul>
+                          <li><img src="img/mantenimiento.png" alt=""></li>
+                          <li>Mantenimiento</li>
+                          <li>Mantengo una comunicación constante con mi cliente, para enseñarle a modificar contenido mediante el panel de Wordpress o para cualquier duda/cambio que quiera hacer en su sitio web.</li>
+                        </ul>
+                      </div>
+                      <div class="item">
+                        <ul>
+                          <li><img src="img/mobile2.png" alt=""></li>
+                          <li>Mobile Priority</li>
+                          <li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo móvil.</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+
+                <section id="blog">
+                  <div class="contenedor">
+                    <h3>Blog</h3>
+                    <div class="contenedor-publicaciones">
+                      <div class="publicacion wow bounceInLeft">
+                        <img src="img/generar-trafico-web.png" alt="generar-trafico-web">
+                        <div class="contenido">
+                          <h4>No alcanza con tener solamente una página web</h4>
+                          <p>¿Quieres aprender cuales son las herramientas que usan las mejores marcas para generar tráfico?</p>
+                          <a href="https://www.mabelcajal.com/2016/08/herramientas-para-generar-buen-trafico-web.html/" target="_blank" class="hvr-forward">Leer más ➝</a>
+                        </div>
+                      </div>
+                      <div class="publicacion wow bounceInRight">
+                        <img src="img/remarketing.jpg" alt="remarketing">
+                        <div class="contenido">
+                          <h4>¿Qué es el remarketing y como utilizarlo?</h4>
+                          <p>El remarketing es una de las técnicas mas utlizadas por las mejores companías! ¿Quieres saber de que se trata?</p>
+                          <a href="https://www.inboundcycle.com/diccionario-marketing-online/remarketing" target="_blank" class="hvr-forward">Leer más ➝</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+
+                <section id="que-esperas">
+                  <div class="contenedor wow lightSpeedIn">
+                    <h3>¿Qué esperas para tener tu página web?</h3>
+                    <a href="mailto:reybec.info@gmail.com" class="hvr-bounce-in">¡Charlemos!</a>
+                  </div>
+                </section>
+                
+                <section id="contacto">
+                  <?php
+                      if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+                      <h3>El mensaje se envio correctamente. Nos contactaremos a la brevedad.</h3>                
+                      <?php
+                      } else{
+                      ?>
+                  <div class="contenedor wow fadeIn">
+                    <h3>Contacto</h3>
+                    <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+                      <input type="text" placeholder="Nombre" name="nombre" required>
+                      <input type="email" placeholder="Email" name="email" required>
+                      <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+                      <input type="submit" value="Enviar Mensaje" class="hvr-bounce-out">
+                    </form>
+                    <?php } ?>  
+
+                    <div class="contacto-info">
+                    <div class="mail"><img src="img/mail-icon.png" alt="">reybec.info@gmail.com</div>
+                    <div class="whatsapp"><img src="img/whatsapp-icon.png" alt="">8180184512</div>
+                    </div>               
+                  </div>
+                </section>
+    </main>
+    
+<footer>
+  <div class="contenedor">
+    <div class="redes">
+      <a href="https://www.facebook.com/reybectech" target="_blank"><img src="img/facebook.png" alt="imagen de facebook"></a>   
+      <a href="https://github.com/jasiel13" target="_blank"><img src="img/github.png"   alt="imagen de github"></a>
+      <a href="https://www.linkedin.com/in/enrique-jasiel-becerra-reyna-55729b152/" target="_blank"><img src="img/linkedin.png" alt="imagen de linkedin"></a>
+    </div>
+    <div class="parrafo">
+      <p>© 2019 Jasiel Becerra | Diseñador Web Torreón,Coahuila. Todos los derechos reservados.</p>
+    </div>
+  </div>
+</footer>
+
+    <script src="jquery/jquery-3.4.1.min.js"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+
+    <script>
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+  </script>
+
+<script>
+$(document).ready(function(){
+   $(window).scroll(function(){
+  scroll = $(window).scrollTop();
+  //crear el scroll 
+  if (scroll > 100) {
+    $('.menu').css({"position":"fixed"});
+    $('.menu').css({"width":"100%"});
+    $('.menu').css({"top":"0"});
+    $('.menu').css({"background":"#fff"});
+    $('.menu a').css({"color":"#000"});
+    $('.logo').css({"color":"#000"});
+    $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+    $('.menu').css({"z-index":"100"});
+
+    //codigo para el efecto hover
+    $('.newcolor').hover(function() {
+    $(this).css({"color":"#550055"});
+    },function(){
+			$(this).css({"color":"#000"});
+    });
+
+  } 
+  //volver a crear el menu original quitar las propiedades que se pusieron en el if
+  else {
+    $('.menu').css({"position":"relative"});
+    $('.menu').css({"background":"transparent"});
+    $('.menu').css({"box-shadow":"0 0 0"});
+    $('.menu a').css({"color":"#fff"});
+    $('.logo').css({"color":"#fff"});   
+    
+    //codigo para el efecto hover
+    $('.newcolor').hover(function(){
+		$(this).css({"color":"#550055"});
+		}, function(){
+			$(this).css({"color":"#fff"});
+		});
+
+  }
+})
+
+//animacion del menu en modo responive
+ $('.menu-icon').click(function(){
+  $('header nav').slideToggle();
+ })
+})
+</script>
+
+<script>
+//codigo para el boton de desplazar pagina hacia arriba
+$(document).ready(function(){ 
+  $('.up').click(function(){
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
+  });
+ 
+  $(window).scroll(function(){
+    if( $(this).scrollTop() > 0 ){
+      $('.up').slideDown(300);
+    } else {
+      $('.ir-up').slideUp(300);
+    }
+  }); 
+});
+</script>
+</body>
+</html>
